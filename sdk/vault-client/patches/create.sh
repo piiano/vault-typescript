b486ee2b1222ca25ac6d49b3b3576f785fb5a4eb
David Erel <david@piiano.com>
2023-10-17 20:13:51 +0800

    adding help to create.sh

--- sdk/vault-client/patches/create.sh
+++ sdk/vault-client/patches/create.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo $1
+# to generate patch:
+# 1. yarn build from vault-client
+# 2. make changes
+# 3. run: ./patches/create.sh new-patch-name
+# (*) Patch name without .patch
+
+echo "Creating patch: $1"
 
 git add -f ./src/generated
 
