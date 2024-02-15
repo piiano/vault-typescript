d374cb7804cbd7c794e193d083d6fc0309c8f4f1
David Erel <david@piiano.com>
2024-02-15 21:50:51 +0200

    adding first SSN use case

---
+++ examples/use-cases/run.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+VAULT_TAG=1.10.2
+
+# check for license key of Vault
+if [ -z $PVAULT_SERVICE_LICENSE ] ; then
+	echo "Please first set environment variable: PVAULT_SERVICE_LICENSE"
+	echo "Obtain a free license here: https://piiano.com/docs/guides/get-started#install-piiano-vault"
+	exit 0
+fi
+
+# check for docker existence
+docker help > /dev/null 2>&1
+if [ $? != 0 ] ; then
+	echo "Running Vault requires Docker."
+	echo "Please install docker first: https://docs.docker.com/get-docker/"
+	exit 0
+fi
+
+# check for docker daemon running
+docker version > /dev/null 2>&1
+if [ $? != 0 ] ; then
+	echo "Cannot connect to the Docker daemon."
+	echo "Is the docker daemon running?"
+	exit 0
+fi
+
+# remove existing vault container
+docker rm -f pvault-dev > /dev/null 2>&1
+
+# run vault container
+docker run --rm --init -d \
+  --name pvault-dev \
+  -p 8123:8123 \
+  -e PVAULT_SERVICE_LICENSE=$PVAULT_SERVICE_LICENSE \
+  -e PVAULT_SENTRY_ENABLE=false \
+  -e PVAULT_LOG_DATADOG_ENABLE=none \
+  piiano/pvault-dev:${VAULT_TAG}
+
+echo "Vault is running at http://localhost:8123"
+
+# define an alias for Vault CLI
+shopt -s expand_aliases
+alias pvault="docker run --rm -i --add-host='host.docker.internal:host-gateway' -v $(pwd):/pwd -w /pwd piiano/pvault-cli:${VAULT_TAG}"
+
+# check for Vault version to ensure it is up and running
+until pvault version > /dev/null 2>&1
+do
+    echo "Waiting for the vault to start ..."
+    sleep 1
+done
+
+# Build
+npm ci
+
+# Run the example
+npm test
---
+++ examples/use-cases/src/start.ts
@@ -0,0 +1,34 @@
+import { VaultClient } from "@piiano/vault-client";
+
+async function main() {
+  const client = new VaultClient({
+    vaultURL: "http://localhost:8123",
+    apiKey: "pvaultauth",
+  });
+
+  await client.collections.addCollection({
+    requestBody: {
+      name: "users",
+      type: "PERSONS",
+      properties: [
+        { name: "name", data_type_name: "NAME" },
+        { name: "ssn", data_type_name: "SSN" },
+      ],
+    },
+  });
+
+  var token = await client.tokens.tokenize({
+    collection: "users",
+    reason: "AppFunctionality",
+    requestBody: [
+      {
+        object: { fields: { name: "john doe", ssn: "444-21-4357" } },
+        type: "deterministic",
+      },
+    ],
+  });
+  console.log(token);
+  // sample output: [ { token_id: '1aca18c7-aaa2-3c59-5418-7546433be09c' } ]
+}
+
+main();
